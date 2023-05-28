6d1fffd9b31c0c01c345764aa189a30f00f34978
DustoTail <dustotail@gmail.com>
2023-05-28 21:09:50 +0900

    tested Grid2D with monobehaviour

---
+++ Assets/Scripts/Test/TestGrid2D.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+using Grid2D;
+
+namespace Test
+{
+    public class TestGrid2D : MonoBehaviour
+    {
+        [SerializeField] private int width;
+        [SerializeField] private int height;
+        private Grid2D<string> _grid;
+        private void Start()
+        {
+            _grid = new Grid2D<string>(width, height);
+            Debug.Log(_grid);
+            for(int y = 0; y < height; y++)
+            {
+                for(int x = 0; x < width; x++)
+                {
+                    _grid.GetCell(x, y);
+                }
+            }
+            _grid.ForEach((x) => { Debug.Log(x); });
+        }
+    }
+}
