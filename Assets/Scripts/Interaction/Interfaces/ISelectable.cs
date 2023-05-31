cbd7e391f92df9d39d45b27fc907c6abc1bd5aad
DustoTail <dustotail@gmail.com>
2023-05-31 22:52:28 +0900

    Added Selector and Selectable behaviour

---
+++ Assets/Scripts/Interaction/Interfaces/ISelectable.cs
@@ -0,0 +1,9 @@
+namespace Interaction
+{
+    public interface ISelectable
+    {
+        public void OnSelectEnter();
+        public void OnSelectStay();
+        public void OnSelectExit();
+    }
+}
---
+++ Assets/Scripts/Interaction/Interfaces/ISelector.cs
@@ -0,0 +1,9 @@
+namespace Interaction
+{
+    public interface ISelector
+    {
+        public ISelectable CurrentSelectable { get; }
+        public ISelectable PreviousSelectable { get; }
+        public void Select(ISelectable currentSelectable);
+    }
+}
