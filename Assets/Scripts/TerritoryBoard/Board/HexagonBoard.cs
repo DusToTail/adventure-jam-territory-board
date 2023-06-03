594c6742f434f35bba0ea6b2cc354e3faa87b4e3
DustoTail <dustotail@gmail.com>
2023-06-03 17:20:11 +0900

    changed 2 local variable names for clarity

--- Assets/Scripts/TerritoryBoard/Board/HexagonBoard.cs
+++ Assets/Scripts/TerritoryBoard/Board/HexagonBoard.cs
@@ -20,14 +20,14 @@ namespace TerritoryBoard
             int width = boardConfig.width;
             int height = boardConfig.height;
             bool forwardOffsetOddHeight = boardConfig.forwardOffsetOddHeight;
-            Vector3 spawnPos = boardConfig.spawnPosition;
+            Vector3 boardSpawnPos = boardConfig.spawnPosition;
             float interval = boardConfig.interval;
 
             float hexagonSize = tileConfig.size;
             float hexagonHeight = tileConfig.height;
             HexagonTile.Config.Pivot pivot = tileConfig.pivot;
 
-            // Procedurally create mesh and material and cache them
+            // Procedurally create mesh and assign material and cache them
             CachedMesh = HexagonTileUtilities.GetMesh(hexagonSize, hexagonHeight, pivot);
             CachedMaterial = new Material(Shader.Find("Universal Render Pipeline/Lit"));
 
@@ -36,15 +36,15 @@ namespace TerritoryBoard
             _grid.ForEach(cell =>
             {
                 bool isForwardOffsetHeight = (forwardOffsetOddHeight && cell.Y % 2 == 1) || (!forwardOffsetOddHeight && cell.Y % 2 == 0);
-                float _x = spawnPos.x + cell.X * HexagonTileUtilities.WidthInterval * hexagonSize + (isForwardOffsetHeight ? HexagonTileUtilities.EdgeDistance * hexagonSize : 0);
-                float _y = spawnPos.y;
-                float _z = spawnPos.z + cell.Y * 1.5f * hexagonSize;
-                Vector3 center = new Vector3(_x,_y,_z);
+                float _x = boardSpawnPos.x + cell.X * HexagonTileUtilities.WidthInterval * hexagonSize + (isForwardOffsetHeight ? HexagonTileUtilities.EdgeDistance * hexagonSize : 0);
+                float _y = boardSpawnPos.y;
+                float _z = boardSpawnPos.z + cell.Y * 1.5f * hexagonSize;
+                Vector3 tilePos = new Vector3(_x,_y,_z);
 
-                var go = Instantiate(prefab, center, Quaternion.identity, transform);
+                var go = Instantiate(prefab, tilePos, Quaternion.identity, transform);
                 var hexTile = go.GetComponent<HexagonTile>();
                 hexTile.SetCell(cell);
-                hexTile.SetDefaultWorldPosition(center);
+                hexTile.SetDefaultWorldPosition(tilePos);
 
                 cell.Value = hexTile;
             });
